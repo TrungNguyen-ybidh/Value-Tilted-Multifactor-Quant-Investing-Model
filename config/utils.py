90bc8d72f1fbccbeb103c07ed1dfd4bd2070918f
TrungNguyen-ybidh <you@example.com>
2026-04-16 19:35:43 -0400

    adding factor data

---
+++ config/utils.py
@@ -0,0 +1,10 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} took {end - start:.2f}s")
+        return result
+    return wrapper
